efd1f1c615384735344c37104791ecc15540d2a6
kkk362 <karankhandelwal123510@gmail.com>
2025-08-17 13:07:46 +0530

    starting new again

--- api/index.js
+++
@@ -1,8 +0,0 @@
-// api/index.js
-const app = require('../app');
-
-// Vercel expects a (req, res) handler.
-// Express provides one via app:
-module.exports = (req, res) => {
-  return app(req, res);
-};
